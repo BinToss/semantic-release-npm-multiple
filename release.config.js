e7d5bac3b1c22164db5db62398a78967dba5e5a9
Noah Sherwin <noahrsherwin@gmail.com>
2026-01-21 01:33:51 -0800

    ci: statically import self in release config

--- release.config.js
+++ release.config.js
@@ -1,10 +1,12 @@
+import npmMultiple from '@bintoss/semantic-release-npm-multiple';
+
 export default {
   branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
-      (await import('@amanda-mitchell/semantic-release-npm-multiple')).default,
+      npmMultiple.default ?? npmMultiple,
       { registries: { github: {}, public: {} } },
     ],
     '@semantic-release/github',
