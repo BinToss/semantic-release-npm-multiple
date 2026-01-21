ec5d3f19e5b412bc79eca506c45d160102e1d849
Noah Sherwin <noahrsherwin@gmail.com>
2026-01-21 00:55:39 -0800

    ci: Import self by name in release config.

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
-      (await import('./index.js')).default,
+      (await import('@amanda-mitchell/semantic-release-npm-multiple')).default,
       { registries: { github: {}, public: {} } },
     ],
     '@semantic-release/github',
